1db09ee4a18f436e1bcf68a4df4460b01d23a85e
dav nguyxn <hoangson091104@gmail.com>
2026-03-01 08:55:07 -0500

    chore: add SOLID refactor note helper

---
+++ refactor-solid-principles-note.js
@@ -0,0 +1,7 @@
+const repoName = 'MetaWave-MP3-App';
+
+function describeSolidRefactorNote() {
+  return `${repoName}: prefer small modules with one clear responsibility.`;
+}
+
+module.exports = { describeSolidRefactorNote };
